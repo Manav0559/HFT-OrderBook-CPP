f7a11bea02ee0e61ed1e010606e31e1926172417
Manav Singla <manavsingla1728@gmail.com>
2026-01-17 15:10:52 +0530

    update fetch_data

--- fetch_data.py
+++ fetch_data.py
@@ -14,21 +14,20 @@ data = response.json()
 filename = "market_data.csv"
 with open(filename, mode='w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(["id", "side", "price", "quantity"]) # Header
+    writer.writerow(["id", "side", "price", "quantity"])
     
     order_id = 1
     
-    # 3. Write Bids (Buyers) - Binance gives strings, we need integers for C++
-    # Example: Price "95000.50" -> 9500050 (Fixed point math)
+    # 3. Write Bids (Buyers)
     for bid in data['bids']:
         price_float = float(bid[0])
         qty_float = float(bid[1])
         
         # Convert to Integer format for HFT (multiply by 100 to keep cents)
         price_int = int(price_float * 100) 
-        qty_int = int(qty_float * 1000) # Assuming 3 decimal places for qty
+        qty_int = int(qty_float * 1000)
         
-        writer.writerow([order_id, 0, price_int, qty_int]) # 0 = Buy
+        writer.writerow([order_id, 0, price_int, qty_int]) 
         order_id += 1
         
     # 4. Write Asks (Sellers)
@@ -39,7 +38,7 @@ with open(filename, mode='w', newline='') as file:
         price_int = int(price_float * 100)
         qty_int = int(qty_float * 1000)
         
-        writer.writerow([order_id, 1, price_int, qty_int]) # 1 = Sell
+        writer.writerow([order_id, 1, price_int, qty_int])
         order_id += 1
 
 print(f"Success! Data saved to {filename}")
